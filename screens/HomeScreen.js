082c4ae1d7a892075c306a394d9f6ef780e35c59
rewell namu <riwenamu@gmail.com>
2025-07-19 23:55:47 +0300

    backcolor

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,7 +3,7 @@ import { View, Text, Button } from 'react-native';
 
 export default function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#1f1c1cff' }}>
       <Text style={{ fontSize: 24, marginBottom: 20 }}>Welcome to QR Sales Tracker</Text>
       <Button title="Scan Item" onPress={() => navigation.navigate('Scan')} />
       <View style={{ height: 10 }} />
